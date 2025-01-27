d7771716e1ac9436b2a918bc01d7a4e75e861939
karripartanen <karripartanen25@gmail.com>
2025-01-27 18:26:50 +0200

    pushed latest updates

--- example-app/src/tests/Upload.test.tsx
+++ example-app/src/tests/Upload.test.tsx
@@ -1,17 +1,9 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import Upload from '../views/Upload';
 
-test('renders h2 headline', () => {
+test('test upload button', () => {
   render(<Upload />);
-  const header = screen.getByRole('heading', {
-      level: 2,
-    })
-  expect(header).toBeDefined();
-});
-
-test('displays uploading notification after button is clicked', () => {
-  render(<Upload />);
-  // simulates clicking the button
-  fireEvent.click(screen.getByRole('button'));
+  fireEvent.click(screen.getByText('Upload'));
   expect(screen.getByText('Uploading...')).toBeDefined();
 });
+
