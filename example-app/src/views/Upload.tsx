5e5fa0cd1796ba7e6ab315b27b1285c45c3b71e5
karripar <karripartanen25@gmail.com>
2025-02-04 12:53:23 +0200

    upload branch

--- example-app/src/views/Upload.tsx
+++ example-app/src/views/Upload.tsx
@@ -1,23 +1,71 @@
 import {useState} from 'react';
 
+// Upload.tsx
 const Upload = () => {
-  const [uploading, setUploading] = useState(false);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+        console.log(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name, e.target.value);
+  };
+
+  const [file, setFile] = useState<File | null>(null);
   return (
-    <>
-      <h2>Upload</h2>
-      <button
-        onClick={() => {
-          setUploading(true);
-          setTimeout(() => {
-            setUploading(false);
-          }, 3000);
-        }}
-      >
-        Upload
-      </button>
-      {uploading && <p>Uploading...</p>}
-    </>
+      <>
+          <h1>Upload</h1>
+          <form onSubmit={handleSubmit}>
+              <div>
+                  <label htmlFor="title">Title</label>
+                  <input
+                      name="title"
+                      type="text"
+                      id="title"
+                      onChange={handleInputChange}
+                  />
+              </div>
+              <div>
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                      name="description"
+                      rows={5}
+                      id="description"
+                      onChange={handleInputChange}
+                  ></textarea>
+              </div>
+              <div>
+                  <label htmlFor="file">File</label>
+                  <input
+                      name="file"
+                      type="file"
+                      id="file"
+                      accept="image/*, video/*"
+                  onChange={handleFileChange}
+                  />
+              </div>
+              <img
+                  src={
+                      file
+                      ? URL.createObjectURL(file)
+                      : 'https://via.placeholder.com/200?text=Choose+image'
+                  }
+                  alt="preview"
+                  width="200"
+              />
+              <button
+                  type="submit"
+                  disabled={file && inputs.title.length > 3 ? false : true}
+              >
+                  Upload
+              </button>
+          </form>
+      </>
   );
 };
-
-export default Upload;
